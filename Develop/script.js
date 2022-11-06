3cd952a2b61fca44e162a43b0bf6904e01ce12d2
Zachary-Rossman <zrossman@yahoo.com>
2022-11-05 19:47:45 -0500

    added arrays, generates 1 number passwords, fully functional with 1 number

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,32 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//Password generator
+// Global Variables
 
-// Write password to the #password input
+//This is my database: Numbers, special characters, lowercase letters, uppercase letters
+let number = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']; //number array with numbers 0-9
+let passwordLength = 5;
+let specialCharacter = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
+let lowercaseLetter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+let uppercaseLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+let randomInt = getRandomArbitrary(10);
+console.log (randomInt);
+
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+//This function generates password and return it to the correct password
+function generatePassword() {
+  // Create a password using random numbers as long as the length 
+  return password;
+}
+
+function getRandomArbitrary(max) {
+  return Math.floor (Math.random() * max);
+}
+
+// Write password to the #password input PART OF STARTER CODE!
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
