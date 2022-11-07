ddf07ba8ff435afce6c30e5704e3892f26d21d01
Zachary-Rossman <zrossman@yahoo.com>
2022-11-07 16:27:07 -0600

    fixed specialCharacter values

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ let passwordLength = 8;
 
 //This is my database: Numbers, special characters, lowercase letters, uppercase letters
 let number = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']; //number array with numbers 0-9
-let specialCharacter = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
+let specialCharacter = ['@','%','+','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
 let lowercaseLetter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
 let uppercaseLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 
