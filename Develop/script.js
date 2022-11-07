c0ec1bed1536daf854643f85542a1a71ebd29a4f
Zachary-Rossman <zrossman@yahoo.com>
2022-11-07 16:26:11 -0600

    organized notes in js; cleaned up code

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,11 @@
-//Password generator
 // Global Variables
+let passwordLength = 8;
 
 //This is my database: Numbers, special characters, lowercase letters, uppercase letters
 let number = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']; //number array with numbers 0-9
 let specialCharacter = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
 let lowercaseLetter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
 let uppercaseLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
-let passwordLength = 8;
-let password = [];
 
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
