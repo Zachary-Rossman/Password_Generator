7de2fb1146a85bb2f3e700549a3bd71efe6d47b0
Zachary-Rossman <zrossman@yahoo.com>
2022-11-10 11:26:57 -0600

    now lets user choose length of password, still only numbers

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,7 @@ let passwordLength = 8;
 
 //This is my database: Numbers, special characters, lowercase letters, uppercase letters
 let number = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']; //number array with numbers 0-9
+//Add each array into password one at a time
 let specialCharacter = ['@','%','+','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
 let lowercaseLetter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
 let uppercaseLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
@@ -12,13 +13,42 @@ let generateBtn = document.querySelector("#generate");
 
 //This function generates password and return it to the correct password
 function generatePassword() {
+  let allChoices = [];
+
+  // Ask user how long password needs to be
+  let passwordLength = window.prompt('Please choose a number between 8-50');
+
+  // Ask user if they want numbers
+  let isNumbers = window.confirm(`Do you want to include numbers?`);
+
+  // Ask user if they want Uppercase
+  let isUppercase = window.confirm('Do you wnat to include uppercase letters?');
+
+  // Ask user if they want lowercase
+  let isLowercase = window.confirm(`Do you want to include lowercase letters?`);
+
+  // Ask user if they want a special character
+  let isSpecialCharacter = window.confirm(`Do you want to include special characters?`);
+
   password = [];
-  for(let i=0;i < passwordLength + 1; i++) {
-    let maximum = number.length -1;
+  if (isNumbers === true) {
+    allChoices = allChoices.concat(number);
+  }
+  if (isUppercase === true) {
+    allChoices = allChoices.concat(uppercaseLetter);
+  }
+  if (isLowercase === true) {
+    allChoices = allChoices.concat(lowercaseLetter);
+  }
+  if (isSpecialCharacter === true) {
+    allChoices = allChoices.concat(specialCharacter);
+  }
+
+  for (let i = 0; i < passwordLength; i++) {
+    let maximum = allChoices.length - 1;
     let minimum = 0
     let rnd = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-    password.push(number[rnd]);
-    console.log(rnd)
+    password.push(allChoices[rnd]);
   }
 }
   // Create a password using random numbers as long as the length 
